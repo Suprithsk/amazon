21c1934f2cd738134b2b8970a0d4d41312f8c9cc
Suprith S K <suprith>
2022-08-31 11:40:34 +0530

    Updates

---
+++ precache-manifest.2b2231524e0e048c2f0aeb952c3e012a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f37f60f25b793ddce9f2",
+    "url": "/amazon/static/css/main.c0c72d2f.chunk.css"
+  },
+  {
+    "revision": "f37f60f25b793ddce9f2",
+    "url": "/amazon/static/js/main.f37f60f2.chunk.js"
+  },
+  {
+    "revision": "5ee476ae1244b517c019",
+    "url": "/amazon/static/js/1.5ee476ae.chunk.js"
+  },
+  {
+    "revision": "57c46bfa80d88e11868a",
+    "url": "/amazon/static/js/2.57c46bfa.chunk.js"
+  },
+  {
+    "revision": "439654e10a20363d9dd7",
+    "url": "/amazon/static/js/runtime~main.439654e1.js"
+  },
+  {
+    "revision": "b6c76f328f5906dc7408a8b0860521bf",
+    "url": "/amazon/index.html"
+  }
+];
